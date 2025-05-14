f0fd0db38f5a7f6cf0ab8b874124995e5fc69348
yyaannn <g853520ck@gmail.com>
2025-05-14 21:59:20 +0800

    Objects can be placed at the specified position in the room

--- Assets/Scripts/RoomManager.cs
+++ Assets/Scripts/RoomManager.cs
@@ -2,23 +2,61 @@ using UnityEngine;
 using Meta.XR.MRUtilityKit;
 using System.Collections;
 using UnityEngine.UIElements;
+using System.Collections.Generic;
+using TMPro;
 
 public class RoomManager : MonoBehaviour
 {
     public MRUKRoom room;
 
-    private void AdjustObjectComponents()
+    public GameObject redCube;
+    public GameObject blueCube;
+
+    public List<GameObject> tables;
+
+    private void SetupTables()
     {
-        Debug.Log(room.gameObject.GetType());
         foreach (Transform child in room.gameObject.transform)
         {
             Debug.Log(child.gameObject.name);
             foreach (Transform grandChild in child)
             {
-                Debug.Log(grandChild.gameObject.name);
-                //grandChild.gameObject.GetComponent<MeshRenderer>().enabled = false;
+                // if (grandChild.gameObject.name == "TABLE_EffectMesh")
+                // {
+                //     tables.Add(grandChild.gameObject);
+                //     Debug.Log(grandChild.gameObject.name);
+                //     var tableNumber = Instantiate(new TextMeshProUGUI(), grandChild.transform);
+                //     tableNumber.text = (tables.IndexOf(grandChild.gameObject) + 1).ToString();
+                //     tableNumber.transform.SetParent(grandChild.transform);
+                //     var newCube = Instantiate(cube, grandChild.transform);
+                //     newCube.transform.SetParent(grandChild.transform);
+                // }
+                if (grandChild.gameObject.name == "TABLE_EffectMesh")
+                {
+                    // Add test cube to tables
+                    var newCube = Instantiate(blueCube, grandChild.transform);
+                    newCube.transform.SetParent(grandChild.transform);
+
+                    // Adjust the position of spawned objects
+                    Bounds bounds = grandChild.GetComponent<Renderer>().bounds;
+                    Vector3 size = bounds.size;
+                    Vector3 offset = new Vector3(size.x / 2f - size.x / 10f, 0, size.z / 2f - size.z / 10f);
+                    newCube.transform.position += offset;
+                }
+                if (grandChild.gameObject.name == "COUCH_EffectMesh")
+                {
+                    // Add test cube to tables
+                    var newCube = Instantiate(redCube, grandChild.transform);
+                    newCube.transform.SetParent(grandChild.transform);
+
+                    // Adjust the position of spawned objects
+                    Bounds bounds = grandChild.GetComponent<Renderer>().bounds;
+                    Vector3 size = bounds.size;
+                    Vector3 offset = new Vector3(0, size.y / 2f, 0);
+                    newCube.transform.position += offset;
+                }
             }
-            
+
         }
     }
 
@@ -33,31 +71,12 @@ public class RoomManager : MonoBehaviour
         Debug.Log("The room is ready");
     }
 
-    private IEnumerable WaitForFloorCreated()
-    {
-        while (true)
-        {
-            var floor = GameObject.Find("FLOOR_EffectMesh");
-            if (floor)
-            {
-                //floor.layer = LayerMask.NameToLayer("Floor");
-                //surface.layerMask = LayerMask.GetMask("Floor");
-                break;
-            }
-            yield return null;
-        }
-        Debug.Log("The mesh of the room is ready");
-    }
-
-
     // Initialize the room
     private IEnumerator Initialization()
     {
         yield return WaitForRoomCreated();
-
-        yield return WaitForFloorCreated();
-
-        AdjustObjectComponents();
+        
+        SetupTables();
     }
 
     public void EnableMRUKManager()
