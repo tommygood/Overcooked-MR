477b1eba8a716a50d6ac0ec514215eb7630e3d4d
yyaannn <g853520ck@gmail.com>
2025-05-28 19:51:37 +0800

    Add order pasting logic

--- Assets/Scripts/OrderPasting.cs
+++ Assets/Scripts/OrderPasting.cs
@@ -1,16 +1,24 @@
+using Oculus.Interaction;
+using Oculus.Interaction.HandGrab;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class OrderPasting : MonoBehaviour
 {
-    void OnCollisionEnter(Collision collision)
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.CompareTag("Order"))
+        {
+            other.GetComponent<Rigidbody>().isKinematic = true;
+        }
     }
 
-    void OnCollisionExit(Collision collision)
+    private void OnTriggerExit(Collider other)
     {
-        
+        if (other.CompareTag("Order"))
+        {
+            other.GetComponent<Rigidbody>().isKinematic = false;
+        }
     }
 }
