7b9833e43e425d59fbf6d2d8a407c5c10a8551cb
Shinny <imshinny0904@gmail.com>
2025-05-28 19:47:54 +0800

    評分
    漢堡評分

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,26 +2,50 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public PlateController plate;
-    public Order currentTopIngredient;
+    public PlateController plate; // 拖入含有 PlateController 的盤子物件
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Z))
         {
-            OnSubmitDish();
+            Debug.Log("Z被按下");
+
+            Order top = FindTopIngredientOnPlate();
+            if (top != null)
+            {
+                Debug.Log("找到最上層食材：" + top.name);
+                plate.CheckRecipeFromTop(top);
+            }
+            else
+            {
+                Debug.Log("未放置任何食材！");
+            }
         }
     }
 
-    public void OnSubmitDish()
+    private Order FindTopIngredientOnPlate()
     {
-        if (currentTopIngredient != null)
+        // 根據盤子位置搜尋周圍區域的食材（要設定 plate transform）
+        Vector3 center = plate.transform.position + Vector3.up * 0.5f;
+        Collider[] colliders = Physics.OverlapBox(center, new Vector3(0.5f, 1f, 0.5f));
+
+        Order top = null;
+        float highestY = float.MinValue;
+
+        foreach (Collider col in colliders)
         {
-            plate.CheckRecipeFromTop(currentTopIngredient);
+            Order ord = col.GetComponent<Order>();
+            if (ord != null)
+            {
+                float y = col.transform.position.y;
+                if (y > highestY)
+                {
+                    highestY = y;
+                    top = ord;
+                }
+            }
         }
-        else
-        {
-            Debug.Log("❗ 尚未放置食材");
-        }
+
+        return top;
     }
 }
