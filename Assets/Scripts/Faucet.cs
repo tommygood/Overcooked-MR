609ce2a00a4ab4669ced4ab799457a0857dcf9de
bogay <pojay11523@gmail.com>
2025-06-01 00:51:56 +0800

    fix(faucet): don't be a NetworkObject

--- Assets/Scripts/Faucet.cs
+++ Assets/Scripts/Faucet.cs
@@ -20,11 +20,9 @@ public class Faucet : MonoBehaviour
     {
         this.waterOriginalScale = this.waterRoot.transform.localScale;
         this.detectZone.OnTriggerStayAsObservable()
-            // .Where(c => c.TryGetComponent<Plate>(out _))
             .Subscribe(_ => this.targetScale = 0.15f)
             .AddTo(this);
         this.detectZone.OnTriggerExitAsObservable()
-            // .Where(c => c.TryGetComponent<Plate>(out _))
             .Subscribe(_ => this.targetScale = 0.001f)
             .AddTo(this);
 
