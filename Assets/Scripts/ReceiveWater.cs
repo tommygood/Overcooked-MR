ce3e3e360aebb888901a8fe1f1e3df9f800830d0
bogay <pojay11523@gmail.com>
2025-06-01 23:21:02 +0800

    feat: add Bowl prefab

---
+++ Assets/Scripts/ReceiveWater.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fusion;
+
+public class ReceiveWater : NetworkBehaviour
+{
+    [SerializeField]
+    private GameObject waterEffect;
+    [Networked]
+    [OnChangedRender(nameof(OnHasWaterChanged))]
+    private bool hasWater { get; set; } = false;
+
+    [Rpc(RpcSources.All, RpcTargets.StateAuthority)]
+    public void Rpc_ReceiveWaterFromFaucet()
+    {
+        this.hasWater = true;
+    }
+
+    public void OnHasWaterChanged()
+    {
+        if (this.hasWater)
+        {
+            Debug.Log("Received water from faucet.");
+            this.waterEffect.SetActive(true);
+        }
+        else
+        {
+            Debug.Log("No water received.");
+            this.waterEffect.SetActive(false);
+        }
+    }
+}
